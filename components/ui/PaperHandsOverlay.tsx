96826edab417c90d5efbb14bf05b61043652b412
Pluviophile <93090356+Pluviophile84@users.noreply.github.com>
2025-11-29 03:11:25 +0100

    Fix import path for utility functions

--- components/ui/PaperHandsOverlay.tsx
+++ components/ui/PaperHandsOverlay.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { cn } from "../lib/utils";
+import { cn } from "../../lib/utils";
 import { TriangleAlert } from "lucide-react";
 
 interface PaperHandsProps {
