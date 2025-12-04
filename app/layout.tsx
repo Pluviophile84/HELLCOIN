f77739f330b36302bf59984c16aea80c420be12a
Pluviophile <93090356+Pluviophile84@users.noreply.github.com>
2025-12-04 20:28:01 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 // 1. Custom Title Font (myfonts8.woff2)
 // Maps to 'font-gothic' in Tailwind
 const customTitle = localFont({
-  src: "./fonts/myfonts2.woff2",
+  src: "./fonts/myfonts3.woff2",
   variable: "--font-pirata", 
   display: "swap",
 });
